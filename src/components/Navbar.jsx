48a842b3241417796759b0854791079223b2ff8c
DVR <derangularamana22@gmail.com>
2024-11-18 08:07:34 +0530

    ham  added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,37 @@
-// src/components/Navbar.jsx
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles.css";
 
 const Navbar = () => {
+  const [isMenuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!isMenuActive);
+  };
+
   return (
     <nav>
       <div className="container">
         <div className="navbar">
           <h1 className="logo">Presently</h1>
-          <div id="nav-links">
+          <div id="nav-links" className={isMenuActive ? "active" : "hidden"}>
             <a href="#">Home</a>
             <a href="#">Resources</a>
             <a href="#">Pricing</a>
             <a href="#">About us</a>
-            <button onClick={() => (window.location.href = "signin.html")} className="btn login">
+            <button
+              onClick={() => (window.location.href = "signin.html")}
+              className="btn login"
+            >
               Login
             </button>
-            <button onClick={() => (window.location.href = "signup.html")} className="btn signup">
+            <button
+              onClick={() => (window.location.href = "signup.html")}
+              className="btn signup"
+            >
               Sign Up
             </button>
           </div>
-          <div>
+          <div onClick={toggleMenu}>
             <span className="material-symbols-outlined" id="hamburger">
               menu
             </span>
